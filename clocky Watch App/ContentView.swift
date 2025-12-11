8ac70dfe3052fbcd024a33dc3adc68a29abe9806
alize <alizesuchon@gmail.com>
2025-12-11 14:59:25 +0100

    Initial commit

---
+++ clocky Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  clocky Watch App
+//
+//  Created by alize suchon on 11/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ clocky Watch App/clockyApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockyApp.swift
+//  clocky Watch App
+//
+//  Created by alize suchon on 11/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct clocky_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
