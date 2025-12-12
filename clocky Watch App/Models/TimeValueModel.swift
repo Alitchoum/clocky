ec9489468a1ba4e80d84aedc7fb897ebc3608717
“Samara <samara.limads@gmail.com>
2025-12-12 10:15:25 +0100

    final touches

--- clocky Watch App/Models/TimeValueModel.swift
+++ clocky Watch App/Models/TimeValueModel.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct TimeValue {
     var minute: Int = 0
-    var second: Int = 50
+    var second: Int = 0
     var hasChosenTime: Bool = false
     
     var totalTime : Int {
